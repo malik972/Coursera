3274f24cc5fac73428e360fde32d3933054edd12
malik972 <69149617+malik972@users.noreply.github.com>
2020-08-31 15:00:02 +0530

    Update SpeakHello.js

--- module3-solution/SpeakHello.js
+++ module3-solution/SpeakHello.js
@@ -1,9 +1,1 @@
-(function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
 
-	window.helloSpeaker = helloSpeaker;
-
-})(window);
